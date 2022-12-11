97a76a62a0a52392e8d9de4f45d5fa5171f6cb4b
kaigabriel12 <kaigabriel12@gmail.com>
2022-12-10 22:12:21 -0300

    ok

--- mysite/static/registro-venda.js
+++ mysite/static/registro-venda.js
@@ -1,5 +1,5 @@
 function menos() {
-    let total = document.querySelector(".m-2")
+    var total = document.querySelector(".m-2")
     console.log(total.textContent)
     if (!total.innerHTML == "0"){
         total.innerHTML = parseInt(total.innerHTML) - 1
@@ -8,8 +8,9 @@ function menos() {
     
 }
 
+
 function mais() {
-    let total = document.querySelector(".m-2")
+    var total = document.querySelector(".m-2")
     if (!total.innerHTML == "0"){
         total.innerHTML = parseInt(total.innerHTML) + 1
 
