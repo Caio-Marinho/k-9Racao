f8e19f5abd9899bec1aff668835fd46736abbe18
kaigabriel12 <kaigabriel12@gmail.com>
2022-12-11 21:15:51 -0300

    ok

---
+++ mysite/static/select.js
@@ -0,0 +1,13 @@
+function selecionar(uf)
+{
+	var mes = document.getElementById("mes");
+	
+	for (var i = 0; i < mes.options.length; i++)
+	{
+		if (mes.options[i].value == uf)
+		{
+			mes.options[i].selected = "true";
+			break;
+		}
+	}
+}
