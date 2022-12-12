a8fd8df53840c7b6cf890e3b980f6b274b0399fb
kaigabriel12 <kaigabriel12@gmail.com>
2022-12-12 09:40:56 -0300

    ok

--- mysite/static/select.js
+++ mysite/static/select.js
@@ -1,10 +1,10 @@
-function selecionar(uf)
+function selecionar(valor)
 {
 	var mes = document.getElementById("mes");
 
 	for (var i = 0; i <= mes.options.length; i++)
 	{
-		if (mes.options[i].value == uf)
+		if (mes.options[i].value == valor)
 		{
 			mes.options[i].selected = "selected" ;
 			break;
