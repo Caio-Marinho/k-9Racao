770497ccd015834990d9dd78fc7f5f869fd916bd
kaigabriel12 <kaigabriel12@gmail.com>
2022-12-13 00:11:25 -0300

    ok

--- mysite/static/submit.js
+++
@@ -1,8 +0,0 @@
-var formID = document.getElementById("incremento");
-var send = $("#plus") && $("#minus");
-
-$(formID).submit(function(event){
-  if (formID.checkValidity()) {
-    send.attr('disabled', 'disabled');
-  }
-});
